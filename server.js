069af26f274254300d1718272c7f9ac0e65587ce
dKcArclite <dKcArclite@users.noreply.github.com>
2018-11-17 11:16:19 -0500

    Add files via upload
    week 9 personal

--- server.js
+++ server.js
@@ -8,51 +8,71 @@ express()
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
 
-    .get('/math', function (request, response) {
-        handleMath(request, response);
+    .get('/calc', function (request, response) {
+        calcRate(request, response);
     })
 
     .get('/', (req, res) => res.render('pages/index'))    
     .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
-    function handleMath(request, response) {
+    function calcRate(request, response) {
         var requestUrl = url.parse(request.url, true);
-        console.log("Query parameters: " + JSON.stringify(requestUrl.query));
+        var mailType = requestUrl.query.mailType;
+        var weight = Number(requestUrl.query.weight);
 
-        var mathFunction = requestUrl.query.mathFunction;
-        var value1 = Number(requestUrl.query.value1);
-        var value2 = Number(requestUrl.query.value2);
-
-        calculate(response, mathFunction, value1, value2);
+        calculateRate(response, mailType, weight);
     }
 
-    function calculate(response, mathFunction, value1, value2) {
-        mathFunction = mathFunction.toLowerCase();
+    function calculateRate(response, mailType, weight) {
+        mailType = mailType.toLowerCase();
+        var result = 0.0;
 
-        var result = 0;
-        var mathSymbol = "";
-        switch (mathFunction) {
-            case "add":
-                result = value1 + value2;
-                mathSymbol = " + ";
+        switch (mailType) {
+            case "stamped":
+                if (weight <= 1.0) result = 0.50;
+                else if (weight <= 2.0) result = 0.71;
+                else if (weight <= 3.0) result = 0.92;
+                else if (weight <= 3.5) result = 1.13;
+                else result = NaN;
                 break;
-            case "subtract":
-                result = value1 - value2;
-                mathSymbol = " - ";
+            case "metered":
+                if (weight <= 1.0) result = 0.47;
+                else if (weight <= 2.0) result = 0.68;
+                else if (weight <= 3.0) result = 0.89;
+                else if (weight <= 3.5) result = 1.10;
+                else result = NaN;
                 break;
-            case "multiply":
-                result = value1 * value2;
-                mathSymbol = " * ";
+            case "flats":
+                if (weight <= 1.0) result = 1.00;
+                else if (weight <= 2.0) result = 1.21;
+                else if (weight <= 3.0) result = 1.42;
+                else if (weight <= 4.0) result = 1.63;
+                else if (weight <= 5.0) result = 1.84;
+                else if (weight <= 6.0) result = 2.05;
+                else if (weight <= 7.0) result = 2.26;
+                else if (weight <= 8.0) result = 2.47;
+                else if (weight <= 9.0) result = 2.68;
+                else if (weight <= 10.0) result = 2.89;
+                else if (weight <= 11.0) result = 3.10;
+                else if (weight <= 12.0) result = 3.31;
+                else if (weight <= 13.0) result = 3.52;
+                else result = NaN;
                 break;
-            case "divide":
-                result = value1 / value2;
-                mathSymbol = " / ";
+            case "retail":
+                if (weight <= 4.0) result = 3.50;
+                else if (weight <= 8.0) result = 3.75;
+                else if (weight <= 9.0) result = 4.10;
+                else if (weight <= 10.0) result = 4.45;
+                else if (weight <= 11.0) result = 4.80;
+                else if (weight <= 12.0) result = 5.15;
+                else if (weight <= 13.0) result = 5.50;
+                else result = NaN;
                 break;
             default:
                 result = NaN;
         }
 
-    var params = { mathSymbol: mathSymbol, value1: value1, value2: value2, result: result };
+    var params = { mailType: mailType, weight: weight, result: result };
 
     response.render('pages/result', params);
 
